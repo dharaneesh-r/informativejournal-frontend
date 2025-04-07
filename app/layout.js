5850393c2c3d0e438311d31045708382878825ad
dharaneesh <dharaneeshr0803@gmail.com>
2025-04-07 23:18:34 +0530

    changes made in frontend-development for speed insights

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
         <footer>
           <Footer />
         </footer>
+        <SpeedInsights />
       </body>
     </html>
   );
